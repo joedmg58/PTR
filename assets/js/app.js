1ed93983d972084337e75baa2e047e1327d3a08a
Joed Machado <joedmg58@gmail.com>
2018-07-02 20:54:33 -0400

    a lot of changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,20 +9,26 @@ var fbDatabaseName = 'ptrdb-jld03e';
 
 $(document).ready( function(){
     
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBcDa7GD5WZ6M3CYC9l40e7RLgAdI2d3Es", //updaed by Jose
-    authDomain: fbDatabaseName + ".firebaseapp.com",
-    databaseURL: "https://ptrdb-jld03e.firebaseio.com",
-    projectId: fbDatabaseName,
-    storageBucket: fbDatabaseName + ".appspot.com",
-    messagingSenderId: "226505053419" //updated by Jose.
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBcDa7GD5WZ6M3CYC9l40e7RLgAdI2d3Es", //updaed by Jose
+        authDomain: fbDatabaseName + ".firebaseapp.com",
+        databaseURL: "https://ptrdb-jld03e.firebaseio.com",
+        projectId: fbDatabaseName,
+        storageBucket: fbDatabaseName + ".appspot.com",
+        messagingSenderId: "226505053419" //updated by Jose.
+    };
+    firebase.initializeApp(config);
 
-  //Reference to ptrdb database
-  var fbptrDB = firebase.database();
-  console.log( fbptrDB );
+    //Reference to ptrdb database
+    var fbptrDB = firebase.database();
+    console.log( fbptrDB );
+
+    //Retrieve Users
+    fbptrDB.ref().on( "child_added", function( snapshot ) {
+        //console.log( snapshot.val() );
+        console.log( snapshot.val().Users );
+    } );
 
   
 
