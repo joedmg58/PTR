b13510e885cf392c44cd1a9060881152e8459091
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-07-02 21:29:25 -0400

    added html code that logs price information for parkwhiz api, and sets parameters for the api according to their documentation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,52 @@
-// PTR: Parking Ticket Reimbursment App
-// Project #1. UM Coding Boot CAmp 2018.
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
 
-//Global Variables
-var userLoginBtn = '#'; // Id of the User login button
-var adminLoginBtn = '#'; //Id of the Admin login button
 
-var fbDatabaseName = 'ptrdb-jld03e';
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment-with-locales.js"></script>
+<script type="text/javascript">
 
-$(document).ready( function(){
-    
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBcDa7GD5WZ6M3CYC9l40e7RLgAdI2d3Es", //updaed by Jose
-    authDomain: fbDatabaseName + ".firebaseapp.com",
-    databaseURL: "https://ptrdb-jld03e.firebaseio.com",
-    projectId: fbDatabaseName,
-    storageBucket: fbDatabaseName + ".appspot.com",
-    messagingSenderId: "226505053419" //updated by Jose.
-  };
-  firebase.initializeApp(config);
+var coordinates = ["26.1181345,-80.1382116"];
+var startTime = ["2018-07-03T12:00:00.000Z"];
+var endTime = ["2018-07-03T22:44:00.000Z"];
+var pract = moment("2013-12-24 14:30").format()
+console.log(pract);
+//moment().toISOString() // 2013-02-04T22:44:30.652Z
 
-  //Reference to ptrdb database
-  var fbptrDB = firebase.database();
-  console.log( fbptrDB );
+function logPrices () {
 
-  
+var queryURL = "http://api.parkwhiz.com/v4/quotes/?q=coordinates:" + coordinates + "&start_time=" + startTime + "&end_time=" + endTime + "&distance=5&api_key=86c499549718c3f35aa39542839060bbc926217d";
 
+
+$.ajax({
+    url:queryURL,
+    method: "GET"
+})
+
+.then(function(response) {
+
+    console.log(queryURL);
+    console.log(response);
+    console.log(response[0].purchase_options[0].price);
+    var max = 0;
+    for (i=0; i<response.length; i++) {
+        console.log(i,response[i].purchase_options[0].price.USD);
+        var price = response[i].purchase_options[0].price.USD;
+        if (price > max) {
+            max = price;
+        }
+    }
+    console.log("Max pricing = ", max);
 });
+};
+
+logPrices();
+
+</script>
+
+</body>
+
+</html>
